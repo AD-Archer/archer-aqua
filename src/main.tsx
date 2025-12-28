5e54cc71d68347d11b91e791c264592b41583891
ad-archer <antonioarcher.dev@gmail.com>
2025-12-28 07:58:18 -0500

    update nextjs and add plausible

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { init } from "@plausible-analytics/tracker";
 import App from "./App.tsx";
 import "./index.css";
+
+init({
+  domain: "aqua.adarcher.app",
+  endpoint: "https://plausible.adarcher.app/api/event",
+  formSubmissions: true,
+  outboundLinks: true,
+});
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
